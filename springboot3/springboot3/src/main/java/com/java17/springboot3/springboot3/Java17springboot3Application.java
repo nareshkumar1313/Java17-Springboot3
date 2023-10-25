a8c84fe998a220e9e34dc55615ba76cf4f95c0a9
naresh_kalakonda <nareshkumar1313@gmail.com>
2023-10-25 20:12:45 +0530

    basic structure

---
+++ springboot3/springboot3/src/main/java/com/java17/springboot3/springboot3/Java17springboot3Application.java
@@ -0,0 +1,13 @@
+package com.java17.springboot3.springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17springboot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java17springboot3Application.class, args);
+	}
+
+}
---
+++ springboot3/springboot3/src/test/java/com/java17/springboot3/springboot3/Java17springboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java17.springboot3.springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17springboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
